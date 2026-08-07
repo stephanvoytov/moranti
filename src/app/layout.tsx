e94570babaf8068775b362ac2d424268c358c4b7
Stepan Voytov <voytov.st.vi@gmail.com>
2026-08-07 09:47:41 +0200

    fix(analytics): убрать ssr:true из init Метрики — с ним tag.js не отправляет хиты, счётчик не работал с самого начала

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -177,7 +177,7 @@ export default async function RootLayout({
           <script
             nonce={nonce}
             dangerouslySetInnerHTML={{
-              __html: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})(window,document,"script","https://mc.yandex.ru/metrika/tag.js","ym");ym(${ymNumber},"init",{ssr:true,ecommerce:"dataLayer",accurateTrackBounce:true,trackLinks:true});`,
+              __html: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})(window,document,"script","https://mc.yandex.ru/metrika/tag.js","ym");ym(${ymNumber},"init",{ecommerce:"dataLayer",accurateTrackBounce:true,trackLinks:true});`,
             }}
           />
         )}
